d42ccdd40efdbd4ad021e4ff34e394db7638ff24
Dshan Test <noobhacker3170@gmail.com>
2024-11-26 16:20:04 -0600

    2 attempt at getting nvim to work

--- neovim.nix
+++ neovim.nix
@@ -4,6 +4,6 @@
     programs.neovim.enable = true;
     programs.neovim.viAlias = true;
     programs.neovim.vimAlias = true;
-    home.file.".config/nvim".source = ./configs/nvim
+    home.file.".config/nvim".source = ./configs/nvim;
     home.file.".config/nvim".recursive = true;
   }
