859009750d402e6a72ca5a06d1a694fefede2766
Dshan Test <noobhacker3170@gmail.com>
2024-11-26 17:44:38 -0600

    fix: got tmux working properly

--- tmux.nix
+++ tmux.nix
@@ -1,5 +1,6 @@
 { config, pkgs, ... }:
 {
    programs.tmux.enable = true;
-   home.file.".config/tmux/tmux.conf".source = ./configs/tmux/tmux.conf;
+   home.file.".config/tmux".source = ./configs/tmux;
+   home.file.".config/tmux".recursive = true;
 }
